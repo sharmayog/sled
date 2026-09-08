6386e25fb0207b1d9223fe7c811d2342ae1480e8
quarg2 <245415063+quarg2@users.noreply.github.com>
2026-09-08 11:34:27 +0530

    Input handling base

---
+++ src/input.h
@@ -0,0 +1,13 @@
+#ifndef SLED_INPUT_H
+#define SLED_INPUT_H
+
+typedef enum
+{
+    INPUT_CAT,
+    INPUT_ADD_LINE,
+    INPUT_DELETE_LINE,
+    INPUT_EDIT_LINE,
+    INPUT_EXIT,
+} Input;
+
+#endif
--- src/sled.c
+++ src/sled.c
@@ -1,8 +1,27 @@
-int main(void)
-{
-    char inputBuffer[128];
+#include <stdio.h>
 
-    getinput();
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        fprintf(stderr, "Supply file name.\n"
+                        "Usage\n"
+                        "sled FILE");
+        return 0;
+    }
+
+    char inputBuffer[128];
+    Input input;
+
+    FILE *fp = fopen(argv[1], "r+");
+    if (!fp)
+    {
+        return 74;
+    }
+
+    while ((input = getinput(inputBuffer)) != INPUT_EXIT)
+    {
+    }
 
     return 0;
 }
