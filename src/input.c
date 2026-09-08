a952f0342339b8414cf6ef9eeb2d5696d8a420f5
142829408+sharmayog@users.noreply.github.com <sharmayogesh3873@gmail.com>
2026-09-08 11:43:56 +0530

    getinput

--- src/input.c
+++ src/input.c
@@ -1,1 +1,25 @@
-#include input.h
+#include "input.h"
+#include <stdio.h>
+
+Input getInput(void) {
+    int c;
+
+    getInput:
+    c = getchar();
+
+    switch (c) {
+        case 'c':
+            return INPUT_CAT;
+            case 'a':
+            return INPUT_ADD_LINE;
+        case 'd':
+            return INPUT_DELETE_LINE;
+        case 'e':
+            return INPUT_EDIT_LINE;
+        case 'q':
+            return INPUT_EXIT;
+        default:
+            fprintf(stderr, "Invalid input: %c\n", c);
+            goto getInput;
+    }
+}
